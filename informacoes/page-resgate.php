1a2317707edd9109cc8ca34cc44eca9087a5db29
Iran Ribeiro <ifreitasribeiro@hotmail.com>
2020-06-21 12:22:08 -0300

    Página de resgate, teste de código

--- informacoes/page-resgate.php
+++ informacoes/page-resgate.php
@@ -7,11 +7,26 @@
 * @since SalveUmGatinho 0.1
 */ ?>
 <?php get_header();?>
-    <div class="container">    
-      <header class="m-4 text-center border-bottom">
-        <h2>Resgate</h2>
-        Teste Resgate
-      </header>  
+    <div class="container">
+      <div class=row>
+        <div class="col-md-12 m-4 text-center">
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+        <?php
+          if (have_posts()):
+            while (have_posts()) : the_post();
+              the_content();
+            endwhile;
+          else:
+            echo '<p>Sorry, no posts matched your criteria.</p>';
+          endif;
+          ?>
+        </div>
+      </div>      
     </div>
   <!-- Fim container -->
 <?php get_footer(); ?>
